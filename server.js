a563f36f78e442b51cba310a72f126b107a26df1
Gunter <Gu.garate@duocuc.cl>
2026-05-20 18:48:20 -0400

    pequeñito4

--- server.js
+++ server.js
@@ -49,7 +49,13 @@ app.get('/api/usuarios', (req, res) => {
             return;
         }
         res.json(results);
-    });
+    res.json(results.map(user => ({
+
+  ...user,
+
+  nombre: user.nombre ? user.nombre.slice(0, 5) : ''
+
+})));});
 });
 
 // POST - Crear un nuevo usuario
